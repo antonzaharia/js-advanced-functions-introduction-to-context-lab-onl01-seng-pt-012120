5529f5cf4e8a3d454bc1cd4b35aa23cdf597a217
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-04 20:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function createEmployeeRecord(arr) {
     firstName: arr[0],
     familyName: arr[1],
     title: arr[2],
-    payPerHour : arr[3]
+    payPerHour : arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
   }
 };
