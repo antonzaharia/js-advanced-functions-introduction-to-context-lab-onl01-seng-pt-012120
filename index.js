e4a8d1907559accb1232d79d7b5b09ac99099652
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-04 21:19:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,3 +37,17 @@ function createTimeOutEvent(record, dateTime) {
   })
   return record;
 }
+function hoursWorkedOnDate(record, date) {
+  reocrd.
+}
+let allWagesFor = function () {
+    let eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
